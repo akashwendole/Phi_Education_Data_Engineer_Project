499152e1112f10acf39359a821ea0038eaf69e19
devilking6369 <akash.wendole3020@gmail.com>
2021-11-13 02:07:39 +0530

    Google Scrape using tkinter GUI

--- Google_scrape/google_scrape_console.py
+++ Google_scrape/google_scrape_console.py
@@ -7,36 +7,35 @@ csv_file = open("google_scrape.csv", "w")
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(["title", "description", "link"])
 
-query = "html"
+query = input("Enter the query")
 url = f"https://www.google.com/search?q={query}&ie=utf-8&oe=utf-8"
 print(url)
 site = requests.get(url).content
 doc = BeautifulSoup(site, "html.parser")
 f = doc.find("div", id="main")
-search_results = {}
-lists = []
+print("Search")
 for i in range(1, len(f) - 1):
     try:
         g = f.find_all(class_="ZINbbc xpd O9g5cc uUPGi")[i]
         title = g.find(class_="BNeawe vvjwJb AP7Wnd").getText()
         description = g.find(class_="BNeawe s3v9rd AP7Wnd").getText()
         link = g.find('a')['href'][7:]
-        search_results[title] = {"link": link, "description": description}
-        lists.append((title,link,description))
+        print(f'\t{title}')
+        print(f'\t\t{link}')
+        print(f'\t\t{description}\n')
         csv_writer.writerow([title, description, link])
     except:
         continue
 
-# print(search_results)
-print(lists)
 # People also ask for
 csv_writer.writerow('')
 csv_writer.writerow('')
 csv_writer.writerow('')
 csv_writer.writerow(['People also ask for'])
+print("\nPeople also asked for:")
 also_ask = f.find_all(class_="Lt3Tzc")
 for query in also_ask:
-    print(query.getText())
+    print('\t', query.getText())
     csv_writer.writerow([query.getText()])
 
 # Related searches
@@ -44,7 +43,8 @@ csv_writer.writerow('')
 csv_writer.writerow('')
 csv_writer.writerow('')
 csv_writer.writerow(['Related searches'])
+print("\n\nRelated Searches")
 related_searches = f.find_all(class_="BNeawe s3v9rd AP7Wnd lRVwie")
 for related in related_searches:
-    print(related.getText())
+    print('\t', related.getText())
     csv_writer.writerow([related.getText()])
